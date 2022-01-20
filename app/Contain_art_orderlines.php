068aa196ab358600b7000a78b574b29d3ac9daa7
CristianAtienza27 <c.atienza@cesjuanpablosegundocadiz.es>
2022-01-20 20:58:28 +0100

    4ª entrega: API/Orders en proceso

---
+++ app/Contain_art_orderlines.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contain_art_orderlines extends Model
+{
+
+    protected $table = "contain_art_orderlines";
+
+    protected $fillable = [
+        'article_id','order_line_id','deleted'
+    ];
+	
+}
